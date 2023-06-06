2093fda5f3a66a6644aee2bc7b549746559ef391
Philasande Bhani <85154876+pbhaninaa@users.noreply.github.com>
2023-06-06 15:43:42 +0200

    Commiting register changes

--- frontend/ScanUI/App.js
+++ frontend/ScanUI/App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+
+import signUp from "./components/User/SignUp";
+import logIn from "./components/User/LogIn";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="signUp" component={signUp} options={{ headerShown: false }}/>
+        <Stack.Screen name="logIn" component={logIn} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- frontend/ScanUI/components/User/SignUp.js
+++ frontend/ScanUI/components/User/SignUp.js
@@ -1,11 +1,131 @@
-import React from "react ";
-import { View, TouchableOpacity, Text } from "react-native";
+import { React, useState } from "react";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  SafeAreaView,
+} from "react-native";
+import Styles from "../Styles";
 
-const signUp = () => {
+const signUp = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const isEmpty = () => {
+    return (
+      name === "" ||
+      surname === "" ||
+      username === "" ||
+      password === "" ||
+      confirmPassword === ""
+    );
+  };
+  const validateLength = () => {
+    if (password.length > 3) {
+      return false;
+    }
+    return true;
+  };
+  const isValidEmail = () => {
+    if (email.includes("@")) {
+      return true;
+    }
+    return false;
+  };
+  const handleSignUp = async () => {
+    try {
+      const response = await fetch("back end endpoint", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, surname, username, password }),
+      });
+
+      if (response.ok) {
+        alert("Signup successful");
+      } else {
+        alert("Signup failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  const save = () => {
+    if (isEmpty(name, surname, username, password, confirmPassword)) {
+      alert("Fill them");
+    } else if (password !== confirmPassword) {
+      alert("Passwords don't match");
+    } else if (validateLength(password)) {
+      alert("Try a stronger password");
+    } else if (!isValidEmail(email)) {
+      alert("Email address must contain @ charecter ");
+    } else {
+      handleSignUp();
+      navigation.navigate("login");
+    }
+  };
   return (
-    <View>
-      <Text>Sign Up Works</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Sign In</Text>
+      </View>
+
+      <TextInput
+        placeholder="Name"
+        onChangeText={(value) => {
+          setName(value);
+        }}
+      />
+      <TextInput
+        placeholder="Surname"
+        onChangeText={(value) => {
+          setSurname(value);
+        }}
+      />
+      <TextInput
+        placeholder="Email"
+        onChangeText={(value) => {
+          setEmail(value);
+        }}
+      />
+      <TextInput
+        placeholder="Username"
+        onChangeText={(value) => {
+          setUsername(value);
+        }}
+      />
+      <TextInput
+        placeholder="Password"
+        onChangeText={(value) => {
+          setPassword(value);
+        }}
+      />
+      <TextInput
+        placeholder="Confirm Password"
+        onChangeText={(value) => {
+          setConfirmPassword(value);
+        }}
+      />
+
+      <TouchableOpacity
+        onPress={() => {
+          save();
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => {
+            save();
+          }}
+        >
+          <Text>Log In</Text>
+        </TouchableOpacity>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 export default signUp;
