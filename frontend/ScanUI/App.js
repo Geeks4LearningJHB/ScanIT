4ea8880cd726e5c3e3ed1d60af3c89a4af0c8680
spha <sphacol@gmail.com>
2023-06-07 03:51:20 +0200

    added two textinput

--- frontend/ScanUI/App.js
+++ frontend/ScanUI/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LogIn from './components/User/LogIn';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <LogIn/>
     </View>
   );
 }
@@ -14,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
   },
 });
--- frontend/ScanUI/components/User/LogIn.js
+++ frontend/ScanUI/components/User/LogIn.js
@@ -1,11 +1,74 @@
-import React from "react ";
-import { View, TouchableOpacity, Text } from "react-native";
+import React from "react";
+import { View, TouchableOpacity, Text,StyleSheet,TextInput } from "react-native";
 
 const logIn = () => {
   return (
-    <View>
-      <Text>Log In Works</Text>
+    <View style={styles.content}>
+      <View style={styles.logo}>
+        <Text style={styles.welcomeText}>Welcome</Text>
+      </View>
+
+      <View style={styles.controlls}>
+      <TextInput style={styles.textInput}/>
+      <TextInput style={styles.textInput}/>
+      <TouchableOpacity style={styles.button}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+        
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  content:{
+      flex:1,
+      margin:25,
+      borderColor:'black',
+      borderRadius:10,
+      borderWidth:2,
+      justifyContent:'center',
+    
+    },
+    logo:{
+      flex:1,
+      borderColor:'red',
+      borderColor:'black',
+      borderRadius:10,
+      borderWidth:2,
+      margin:15,
+      alignItems:'center',
+      justifyContent:'center'
+
+    },
+    controlls:{
+      flex:3,
+      borderColor:'red',
+      borderColor:'black',
+      borderRadius:10,
+      borderWidth:2,
+      margin:15,
+      alignItems:'center',
+      justifyContent:'',
+    
+      marginBottom:20,
+
+    },
+    welcomeText:{
+      fontSize:20,
+      fontWeight:'bold',
+      
+    },
+    textInput:{
+      borderWidth:2,
+      borderColor:'black',
+      borderRadius:10,
+      width:250,
+      marginBottom:20,
+      marginHorizontal:0,
+      padding:5,
+    }
+
+    
+})
 export default logIn;
